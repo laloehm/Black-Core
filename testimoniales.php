eeb2f7061228cd5d868655fcd66f803390c84fc0
Eduardo Hernández <34753625+laloehm@users.noreply.github.com>
2019-08-09 09:05:57 -0500

    Add files via upload

---
+++ testimoniales.php
@@ -0,0 +1,134 @@
+<div class="row box-testimonials d-flex justify-content-center align-items-center">
+    <div class="container">
+        <h2 class="mr-auto ml-md-5 titulos w-28 mb-5">TESTIMONIALES.</h2>
+        <div class="row justify-content-center my-5">
+            <!--  -->
+            <div class="col-xl-6 d-flex align-items-center justify-content-center flex-column mb-2 pb-5 pb-sm-0">
+                <div id="carouselTestimonials" class="carousel slide mt-lg-5 d-flex justify-content-center align-items-center w-100" data-ride="carousel" data-interval="80000000">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-md-3 p-0 my-3 my-md-0 d-flex justify-content-center align-items-center">
+                                    <img src="images/Testimoniales/3.png" class="img-fluid" width="200" alt="">
+                                </div>
+                                <div class="col-md-7 d-flex justify-content-center flex-column p-testimonial">
+                                    <div class="row">
+                                        <p class="">Gracias por la asesor&iacutea Black Core, a seguir ganando.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-md-3 p-0 my-3 my-md-0 d-flex justify-content-center align-items-center">
+                                    <img src="images/Testimoniales/5.png" class="img-fluid" width="200" alt="">
+                                </div>
+                                <div class="col-md-7 d-flex justify-content-center flex-column p-testimonial">
+                                    <div class="row">
+                                        <p class="">Algo me queda claro, no es cuesti&oacuten de suerte sino de inteligencia.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-md-3 p-0 my-3 my-md-0 d-flex justify-content-center align-items-center">
+                                    <img src="images/Testimoniales/2.png" class="img-fluid" width="200" alt="">
+                                </div>
+                                <div class="col-md-7 d-flex justify-content-center flex-column p-testimonial">
+                                    <div class="row">
+                                        <p class="">Die Zukunft ist klar. Vielen Dank. <br></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-md-3 p-0 my-3 my-md-0 d-flex justify-content-center align-items-center">
+                                    <img src="images/Testimoniales/1.png" class="img-fluid" width="200" alt="">
+                                </div>
+                                <div class="col-md-7 d-flex justify-content-center flex-column p-testimonial">
+                                    <div class="row">
+                                        <p class="">Easy to invest, easy to win. <br></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-md-3 p-0 my-3 my-md-0 d-flex justify-content-center align-items-center">
+                                    <img src="images/Testimoniales/4.png" class="img-fluid" width="200" alt="">
+                                </div>
+                                <div class="col-md-7 d-flex justify-content-center flex-column p-testimonial">
+                                    <div class="row">
+                                        <p class="">A melhor maneira de investir e ganhar dinheiro.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselTestimonials" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"><i class="fas fa-angle-left"></i></span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselTestimonials" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"><i class="fas fa-angle-right"></i></span>
+                    </a>
+                </div>
+            </div>
+            <!--  -->
+            <div class="col-xl-6 mb-2 px-lg-5" id="testimoniales">
+                <img src="images/Testimoniales/Abre.png" class="img-fluid abre" alt="">
+                <div class="py-4 mb-2 ranking-title">
+                    <h3 class=""> RANKING.</h3>
+                    <img src="images/Testimoniales/Placa-Titulo.png" class="img-fluid" alt="">
+                </div>
+                <div class="pl-3 pr-0">
+                    <table class="table">
+                        <thead class="my-1">
+                            <tr class="table-title">
+                                <th class="avatar"></th>
+                                <th class="posicion-primeros text-red">PUESTO</th>
+                                <th class="borde-primero text-red">NICKNAME</th>
+                                <th class="borde-primero p-0 text-red">CONVERSIÓN</th>
+                            </tr>
+                            <tr class="espacio"></tr>
+                        </thead>
+                        <thead class="my-1">
+                            <tr class="primero">
+                                <th class="avatar"><img src="images/Testimoniales/1-.png" class="img-fluid" alt=""></th>
+                                <th class="posicion-primeros">1</th>
+                                <th>DatMig</th>
+                                <th class="borde-primero">96%</th>
+                            </tr>
+                            <tr class="espacio"></tr>
+                        </thead>
+                        <tbody>
+                            <tr class="segundo">
+                                <th class="avatar"><img src="images/Testimoniales/2-.png" class="img-fluid" alt=""></th>
+                                <td class="posicion-primeros">2</td>
+                                <td>Howen</td>
+                                <td>93%</td>
+                            </tr>
+                            <tr class="espacio"></tr>
+                            <tr class="cuarto">
+                                <td class="avatar"><img src="images/Testimoniales/3-.png" class="img-fluid" width="60" alt=""></td>
+                                <td class="">3</td>
+                                <td>Gabo</td>
+                                <td>89%</td>
+                            </tr>
+                            <tr class="tercero">
+                                <td class="avatar"><img src="images/Testimoniales/5.png" class="img-fluid" width="60" alt=""></td>
+                                <td class="">2</td>
+                                <td>Arius</td>
+                                <td>89%</td>
+                            </tr>
+                            
+                            <tr class="espacio"></tr>
+                        </tbody>
+                    </table>
+                </div>
+                <img src="images/Testimoniales/Cierra.png" class="img-fluid cierra" alt="">
+            </div>
+        </div>
+    </div>
+</div>
